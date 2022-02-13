b0c72a87cde1b78783e7f64c5e80f3dbb7295eef
Proloycb <proloychacroborty@gmail.com>
2022-02-13 14:09:09 +0600

    first commit

---
+++ change.js
@@ -0,0 +1,4 @@
+document.getElementById('change-button').addEventListener('click', function(){
+    const getChange = document.getElementById('change-text');
+    getChange.innerText = 'I AM PENGUINS' ;
+})
